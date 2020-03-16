f37f59c326b9f7e94a9d209c66dd14ba04bcf0e6
Zafer Balkan <zafer@zaferbalkan.com>
2020-03-16 11:54:29 +0300

    Converted test method to DataTestMethod and covered all cases

--- DTG.Tests/DateTimeGroupTests.cs
+++ DTG.Tests/DateTimeGroupTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace DTG.Tests
@@ -7,25 +8,47 @@ namespace DTG.Tests
     [TestClass]
     public class DateTimeGroupTests
     {
-        [TestMethod]
-        public void TestForZulu()
+        [DataTestMethod]
+        [DynamicData(nameof(Data), DynamicDataSourceType.Property)]
+        public void TestForData(string name, string abbr, int offset)
         {
-            var dtg = new DateTimeGroup(DateTime.Now, TimeZoneInfo.Utc);
+            var dtg = new DateTimeGroup(DateTime.Now, TimeZoneInfo.GetSystemTimeZones().ToList().FirstOrDefault(tz => tz.BaseUtcOffset.Hours == offset));
 
-            Assert.AreEqual("Z", dtg.MilitaryTimeZone.Abbreviation);
-            Assert.AreEqual("Zulu", dtg.MilitaryTimeZone.TimeZoneName);
-            Assert.AreEqual(0, dtg.MilitaryTimeZone.Offset);
+            Assert.AreEqual(abbr, dtg.MilitaryTimeZone.Abbreviation);
+            Assert.AreEqual(name, dtg.MilitaryTimeZone.TimeZoneName);
+            Assert.AreEqual(offset, dtg.MilitaryTimeZone.Offset);
         }
 
-
-        [TestMethod]
-        public void TestForAlpha()
+        public static IEnumerable<object[]> Data
         {
-            var dtg = new DateTimeGroup(DateTime.Now, TimeZoneInfo.GetSystemTimeZones().ToList().FirstOrDefault(tz => tz.BaseUtcOffset.Hours == 1));
-
-            Assert.AreEqual("A", dtg.MilitaryTimeZone.Abbreviation);
-            Assert.AreEqual("Alpha", dtg.MilitaryTimeZone.TimeZoneName);
-            Assert.AreEqual(1, dtg.MilitaryTimeZone.Offset);
+            get
+            {
+                yield return new object[] { "Alpha", "A", 1, };
+                yield return new object[] { "Bravo", "B", 2, };
+                yield return new object[] { "Charlie", "C", 3, };
+                yield return new object[] { "Delta", "D", 4, };
+                yield return new object[] { "Echo", "E", 5, };
+                yield return new object[] { "Foxtrot", "F", 6, };
+                yield return new object[] { "Golf", "G", 7, };
+                yield return new object[] { "Hotel", "H", 8, };
+                yield return new object[] { "India", "I", 9, };
+                yield return new object[] { "Kilo", "K", 10, };
+                yield return new object[] { "Lima", "L", 11, };
+                yield return new object[] { "Mike", "M", 12, };
+                yield return new object[] { "November", "N", -1, };
+                yield return new object[] { "Oscar", "O", -2, };
+                yield return new object[] { "Papa", "P", -3, };
+                yield return new object[] { "Quebec", "Q", -4, };
+                yield return new object[] { "Romeo", "R", -5, };
+                yield return new object[] { "Sierra", "S", -6, };
+                yield return new object[] { "Tango", "T", -7, };
+                yield return new object[] { "Uniform", "U", -8, };
+                yield return new object[] { "Victor", "V", -9, };
+                yield return new object[] { "Whiskey", "W", -10, };
+                yield return new object[] { "X-ray", "X", -11, };
+                yield return new object[] { "Yankee", "Y", -12, };
+                yield return new object[] { "Zulu", "Z", 0 };
+            }
         }
     }
 }
